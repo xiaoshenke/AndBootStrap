01df6666131576905f9c6e2f9cd86e8e08ff6908
xiaoshenke <1205914137@qq.com>
2017-03-03 11:34:41 +0800

    add rxjava mvp

--- lib/src/main/java/wuxian/me/andbootstrap/Main.java
+++
@@ -1,8 +0,0 @@
-package wuxian.me.andbootstrap;
-
-/**
- * Created by wuxian on 2/3/2017.
- */
-
-public class Main {
-}
---
+++ lib/src/main/java/wuxian/me/andbootstrap/scheduler/BaseSchedulerProvider.java
@@ -0,0 +1,20 @@
+package wuxian.me.andbootstrap.scheduler;
+
+import android.support.annotation.NonNull;
+
+import io.reactivex.Scheduler;
+
+/**
+ * Allow providing different types of {@link Scheduler}s.
+ */
+public interface BaseSchedulerProvider {
+
+    @NonNull
+    Scheduler computation();
+
+    @NonNull
+    Scheduler io();
+
+    @NonNull
+    Scheduler ui();
+}
