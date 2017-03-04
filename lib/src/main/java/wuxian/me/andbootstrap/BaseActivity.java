10ee9d6679bf0ca117d0b53888d40a28920f5688
xiaoshenke <1205914137@qq.com>
2017-03-04 18:01:15 +0800

    add base activity

---
+++ lib/src/main/java/wuxian/me/andbootstrap/BaseActivity.java
@@ -0,0 +1,37 @@
+package wuxian.me.andbootstrap;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by wuxian on 4/3/2017.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private Toast mToast = null;
+
+    protected void showToast(String msg, int duration) {
+
+        if (mToast == null) {
+            mToast = Toast.makeText(this, msg, duration);
+        } else {
+            mToast.setDuration(duration);
+            mToast.setText(msg);
+        }
+
+        mToast.show();
+    }
+
+    protected void hideKeyboard(EditText edit){
+        if (edit == null) {
+            return;
+        }
+        edit.clearFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(edit.getWindowToken(), 0);
+    }
+}
